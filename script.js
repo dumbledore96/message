17f5a3c3a2fc3bb2d35cf1811839d780635dca64
YaoDumbledore <95558265+dumbledore96@users.noreply.github.com>
2022-01-28 11:05:55 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function messagetest(message , main){
+    var text = document.getElementById("visible").innerHTML;
+    event.preventDefault();
+    console.log(text);
+       text = "<h3>" +message+ "</h3>" +main+"<hr>"+text;
+    document.getElementById("visible").innerHTML = text;
+    document.getElementById("main").value = "";
+    document.getElementById("message").value = "";
+    }
+    
