97c61e1f03e4ab099eda9f4f9025abb6f042912e
YaoDumbledore <95558265+dumbledore96@users.noreply.github.com>
2022-01-28 11:47:57 +0000

    new

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 function messagetest(message , main){
     var text = document.getElementById("visible").innerHTML;
     event.preventDefault();
-    console.log(text);
-       text = "<h3>" +message+ "</h3>" +main+"<hr>"+text;
+    text = "<h3>" +message+ "</h3>" +main+"<hr>"+text;
     document.getElementById("visible").innerHTML = text;
     document.getElementById("main").value = "";
     document.getElementById("message").value = "";
